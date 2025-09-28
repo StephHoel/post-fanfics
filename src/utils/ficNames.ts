b421bb4cd75883fb6cde418c56f2cefd231e1bc5
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-09-28 17:23:05 -0300

    feat: atualiza pacote astro + melhora configs

--- src/utils/ficNames.ts
+++ src/utils/ficNames.ts
@@ -1,11 +1,12 @@
 export const ficNames: Record<string, string> = {
-  // "alem-dos-holofotes": "Além dos Holofotes",
+  "alem-dos-holofotes": "Além dos Holofotes",
   "bastidores-sentimento": "Bastidores de Um Sentimento",
-  // "destinos-cruzados": "Destinos Cruzados",
-  // "entre-codigos-e-cameras": "Entre Códigos e Câmeras",
+  "destinos-cruzados": "Destinos Cruzados",
+  "entre-codigos-e-cameras": "Entre Códigos e Câmeras",
   "familia-conflitos-amores": "Família, Conflitos e Amores",
+  "fora-roteiro": "Fora do Roteiro",
+  "possiveis-ones": "Possíveis One-Shots",
+
   "era-das-chamas": "EAU – Era das Chamas",
   "pulso-lyara": "EAU – Era das Chamas – Livro II: O Pulso de Lyara",
-  // "fora-roteiro": "Fora do Roteiro",
-  // "possiveis-ones": "Possíveis One-Shots",
 }
