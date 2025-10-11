77adae3838fdac932583e254ce9736f872ff1a45
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-10-11 00:58:25 -0300

    feat(astro): adiciona novas fanfics no catálogo

--- src/utils/ficNames.ts
+++ src/utils/ficNames.ts
@@ -1,11 +1,13 @@
 export const ficNames: Record<string, string> = {
   "alem-dos-holofotes": "Além dos Holofotes",
   "bastidores-sentimento": "Bastidores de Um Sentimento",
+  "derrubando-muros": "Derrubando Muros",
   "destinos-cruzados": "Destinos Cruzados",
   "entre-codigos-e-cameras": "Entre Códigos e Câmeras",
   "familia-conflitos-amores": "Família, Conflitos e Amores",
   "fora-roteiro": "Fora do Roteiro",
   "possiveis-ones": "Possíveis One-Shots",
+  "sangue-do-meu-sangue": "Sangue do Meu Sangue",
 
   "era-das-chamas": "EAU – Era das Chamas",
   "pulso-lyara": "EAU – Era das Chamas – Livro II: O Pulso de Lyara",
