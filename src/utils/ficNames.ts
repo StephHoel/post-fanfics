efe430b9fe2cb10cf4dd248c65b73edd0766091d
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-10-11 00:58:25 -0300

    feat(astro): adiciona novas fanfics no catálogo

--- src/utils/ficNames.ts
+++ src/utils/ficNames.ts
@@ -1,9 +1,11 @@
 export const ficNames: Record<string, string> = {
   "alem-dos-holofotes": "Além dos Holofotes",
   "bastidores-sentimento": "Bastidores de Um Sentimento",
+  "derrubando-muros": "Derrubando Muros",
   "destinos-cruzados": "Destinos Cruzados",
   "entre-codigos-e-cameras": "Entre Códigos e Câmeras",
   "familia-conflitos-amores": "Família, Conflitos e Amores",
   "fora-roteiro": "Fora do Roteiro",
   "possiveis-ones": "Possíveis One-Shots",
+  "sangue-do-meu-sangue": "Sangue do Meu Sangue",
 }
