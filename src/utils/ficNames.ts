95cdece98cdf75495104cd21dbde21e9b3da4ace
Steph Hoel <53956021+StephHoel@users.noreply.github.com>
2025-11-01 18:32:23 -0300

    feat: :sparkles: adiciona nova fanfic "O Dia Seguinte"

--- src/utils/ficNames.ts
+++ src/utils/ficNames.ts
@@ -3,6 +3,7 @@ export const ficNames: Record<string, string> = {
   "bastidores-sentimento": "Bastidores de Um Sentimento",
   "derrubando-muros": "Derrubando Muros",
   "destinos-cruzados": "Destinos Cruzados",
+  "dia-seguinte": "O Dia Seguinte",
   "entre-codigos-e-cameras": "Entre Códigos e Câmeras",
   "familia-conflitos-amores": "Família, Conflitos e Amores",
   "fora-roteiro": "Fora do Roteiro",
